2914395d1ea64b295467e78ac9a240ffd40d3f5f
lvpiao <lvpiao@blizzmi.com>
2020-09-21 17:42:01 +0800

    项目基础文件

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 #[macro_use]
 extern crate diesel;
-
 extern crate dotenv;
 extern crate crypto;
+extern crate uuid;
+extern crate chrono;
 extern crate serde;
+
 
 pub mod models;
 pub mod schema;
 
 use diesel::prelude::*;
 use dotenv::dotenv;
-
+use std::env;
 use std::collections::HashMap;
 use std::collections::hash_map::RandomState;
 use crypto::sha2::Sha256;
@@ -19,78 +21,158 @@ use uuid::Uuid;
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use serde::export::fmt::Debug;
-use std::env;
+use models::Word;
+use schema::words::dsl::*;
 
+pub trait Searchable {
+    fn search(&self, word: String) -> Vec<String>;
+}
+
+pub trait Supervisable {
+    //初始化
+    fn initialize() -> Supervisor;
+    //检查输入的释义是否和焦点单词的释义相同
+    fn exam(&self, explanation: &str) -> bool;
+    //向前迭代一个单词
+    fn forward(&mut self);
+    //后退一个单词
+    fn backward(&mut self);
+    //获取当前单词的内容
+    fn get_current_word(&self) -> Option<&Word>;
+    //是否已经迭代完毕
+    fn is_finished(&self) -> bool;
+    //将当前单词设置为已复习状态
+    fn set_word_reviewed_once(&self);
+    //
+    fn is_reviewable(&self)->bool;
+}
+
+pub struct Supervisor {
+    tasks: Vec<Word>,
+    cursor: usize,
+    conn: SqliteConnection,
+}
+
+impl Supervisable for Supervisor {
+    fn initialize() -> Supervisor {
+        let conn = establish_connection();
+        let word_results = words.filter(status.gt(-1)).limit(10).load::<Word>(&conn).expect
+        ("查询出错了");
+        return Supervisor {
+            tasks: word_results,
+            cursor: 0,
+            conn,
+        };
+    }
+
+    fn exam(&self, explanation: &str) -> bool {
+        if let Some(current_word) = self.tasks.get(self.cursor) {
+            let explanations: Vec<String> = serde_json::from_str(&current_word.exp_cn).unwrap();
+            return explanations.contains(&String::from(explanation));
+        }
+        return false;
+    }
+
+    fn forward(&mut self) {
+        self.cursor += 1;
+    }
+
+    fn backward(&mut self) {
+        self.cursor -= 1;
+    }
+
+
+    fn get_current_word(&self) -> Option<&Word> {
+        return self.tasks.get(self.cursor);
+    }
+
+    fn is_finished(&self) -> bool {
+        return self.cursor == self.tasks.len() - 1;
+    }
+
+    fn set_word_reviewed_once(&self) {
+        let current_word = self.tasks.get(self.cursor).expect(format!("程序出错,无法找到单词,游标位置:{}", self.cursor).as_str());
+        //如果status <=3 那么就status
+        if current_word.status < 3 {
+            diesel::update(words.find(current_word.id)).set(status.eq(status + 1)).execute(&self
+                .conn).unwrap();
+        } else {
+            diesel::update(words.find(current_word.id)).set(status.eq(-1)).execute(&self.conn).unwrap();
+        }
+        //todo 这里直接用unwrap不大合适，需要进行错误处理
+    }
+
+    fn is_reviewable(&self) -> bool {
+        return self.tasks.len() > 0;
+    }
+}
 
 #[derive(Serialize, Deserialize)]
-struct YoudaoResponse{
-    #[serde(rename="returnPhrase")]
+struct YoudaoResponse {
+    #[serde(rename = "returnPhrase")]
     return_phrase: Vec<String>,
     query: String,
-    #[serde(rename="errorCode")]
+    #[serde(rename = "errorCode")]
     error_code: String,
     l: String,
-    #[serde(rename="tSpeakUrl")]
+    #[serde(rename = "tSpeakUrl")]
     t_speak_url: String,
     web: Vec<YoudaoValues>,
-    #[serde(rename="requestId")]
+    #[serde(rename = "requestId")]
     request_id: String,
     translation: Vec<String>,
     dict: YoudaoDict,
     basic: YoudaoBasic,
-    #[serde(rename="isWord")]
+    #[serde(rename = "isWord")]
     is_word: bool,
-    #[serde(rename="speakUrl")]
+    #[serde(rename = "speakUrl")]
     speak_url: String,
 
 }
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
-struct YoudaoValues{
-    value :Vec<String>,
+struct YoudaoValues {
+    value: Vec<String>,
     key: String,
 }
 
 #[derive(Serialize, Deserialize)]
-struct YoudaoDict{
+struct YoudaoDict {
     url: String,
 }
 
 #[derive(Serialize, Deserialize)]
-struct YoudaoWebDict{
+struct YoudaoWebDict {
     url: String,
 }
 
 #[derive(Serialize, Deserialize)]
-struct YoudaoBasic{
+struct YoudaoBasic {
     explains: Vec<String>,
 }
 
 
-
-pub struct YoudaoSearcher{
+pub struct YoudaoSearcher {
     config: YoudaoConfig,
 }
 
-pub struct YoudaoConfig{
+pub struct YoudaoConfig {
     pub appkey: String,
     pub appsecret: String,
 }
 
-pub trait Searchable{
-    fn search(&self, word: String)->Vec<String>;
-}
 
-impl Searchable for YoudaoSearcher{
+impl Searchable for YoudaoSearcher {
     fn search(&self, word: String) -> Vec<String> {
         let params: HashMap<&str, String> = self.build_params_by_word(word.clone());
         let response_text = self.exec_query(params);
-        let youdao_response:YoudaoResponse = serde_json::from_str(response_text.as_str()).unwrap();
+        // println!("接口返回数据是:{}", &response_text);
+        let youdao_response: YoudaoResponse = serde_json::from_str(response_text.as_str()).unwrap();
         let values_iter = youdao_response.web.iter();
         for val in values_iter {
             if word.eq(&val.key.to_lowercase()) {
-                return  val.value.clone()
+                return val.value.clone();
             }
         }
 
@@ -98,27 +180,27 @@ impl Searchable for YoudaoSearcher{
     }
 }
 
-impl YoudaoSearcher{
-    pub fn new(config :YoudaoConfig)->YoudaoSearcher{
-        YoudaoSearcher{
+impl YoudaoSearcher {
+    pub fn new(config: YoudaoConfig) -> YoudaoSearcher {
+        YoudaoSearcher {
             config,
         }
     }
 
 
-    fn build_sign(salt : &str, word :&str, app_key :&str, cur_time :&str, app_sec :&str)
-                ->String{
-        let mut base_str:String  = String::from(app_key);
+    fn build_sign(salt: &str, word: &str, app_key: &str, cur_time: &str, app_sec: &str)
+                  -> String {
+        let mut base_str: String = String::from(app_key);
         base_str.push_str(word);
         base_str.push_str(salt);
         base_str.push_str(cur_time);
         base_str.push_str(app_sec);
-        let mut sha =  Sha256::new();
+        let mut sha = Sha256::new();
         sha.input_str(base_str.as_str());
-        return sha.result_str()
+        return sha.result_str();
     }
 
-    fn build_params_by_word(&self, word :String) -> HashMap<&'static str, String, RandomState> {
+    fn build_params_by_word(&self, word: String) -> HashMap<&'static str, String, RandomState> {
         let mut map = HashMap::new();
         let salt = Uuid::new_v4().to_string();
         let cur_time = Utc::now().timestamp().to_string();
@@ -129,10 +211,10 @@ impl YoudaoSearcher{
             self.config.appkey.as_str(),
             cur_time.as_str(),
             //"gTzzXsjEpFX9wUd8Rrio5SXfgzlcqq56"
-            self.config.appsecret.as_str()
-            )
+            self.config.appsecret.as_str(),
+        ),
         );
-        map.insert("q", String::from("hello"));
+        map.insert("q", word);
         map.insert("from", String::from("zh-CHS"));
         map.insert("to", String::from("en"));
         map.insert("appKey", String::from("2f3a48a702316da0"));
@@ -140,20 +222,16 @@ impl YoudaoSearcher{
         map.insert("signType", String::from("v3"));
         map.insert("curtime", cur_time);
         return map;
-
     }
 
-    fn exec_query(&self, params: HashMap<&str, String>)->String{
+    fn exec_query(&self, params: HashMap<&str, String>) -> String {
         let client = reqwest::blocking::Client::new();
         // let params = [("foo", "bar"),("barz", "quux")]
         let res = client.post("https://openapi.youdao.com/api").form(&params).send()
             .unwrap().text().unwrap();
         return res;
     }
-
-
 }
-
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
@@ -161,3 +239,4 @@ pub fn establish_connection() -> SqliteConnection {
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
+
---
+++ src/main.rs
@@ -0,0 +1,136 @@
+extern crate diesel;
+extern crate ragger_api;
+extern crate clap;
+
+use self::diesel::prelude::*;
+use self::ragger_api::models::*;
+use self::ragger_api::*;
+use clap::App;
+use std::io::{stdin, stdout, Write};
+
+// #[derive(StructOpt)]
+// struct Cli{
+//     operation: String,
+//     word: String,
+// }
+
+
+fn main() {
+    // let args = Cli::from_args();
+    // println!("参数是: operation: {}, word: {}", &args.operation, &args.word);
+
+    let matches = App::new("ragger")
+        .version("1.0")
+        .author("Lv Piao 1243194544@qq.com")
+        .about("单词复习功能")
+        .arg("<operation> 操作:query/review")
+        .arg("[param] 操作参数:如果是query则为要查询的单词")
+        .get_matches();
+    if let Some(o) = matches.value_of("operation") {
+        if o.eq("query") {
+            if let Some(p) = matches.value_of("param") {
+                let youdao_searcher = YoudaoSearcher::new(YoudaoConfig {
+                    appkey: String::from("2f3a48a702316da0"),
+                    appsecret: String::from("gTzzXsjEpFX9wUd8Rrio5SXfgzlcqq56"),
+                });
+                let values = youdao_searcher.search(String::from(p));
+                println!("单词的释义是: {:?}", values);
+            } else {
+                println!("请输入param参数");
+            }
+        } else if o.eq("review") {
+            //整理命令
+            //_exit 退出复习
+            //_skip 跳过当前复习的单词
+            //_remind
+            let mut supervisor = Supervisor::initialize();
+            if !supervisor.is_reviewable(){
+                println!("当前没有可供复习的单词!");
+                return;
+            }
+            println!("确认开始复习吗?(Y/N)");
+            loop {
+                let mut confirmation = String::new();
+                stdin().read_line(&mut confirmation).unwrap();
+                match confirmation.trim() {
+                    "Y"=>{
+                        println!("接下来将开始复习, 请按照控制台输出的单词输入其释义");
+                        break;
+                    },
+                    "N"=>return,
+                    _ =>println!("请输入'Y'来确认开始复习或者输入'N'退出复习")
+
+                }
+            }
+            //todo 如果vec的长度为空则不用循环了
+            // println!(">确认开始复习吗? Y/N");
+            loop {
+                if let Some(current_word) = supervisor.get_current_word(){
+                    match stdout().flush() {
+                        Err(_e) => panic!("刷新输出失败"),
+                        _ => {}
+                    }
+                    println!("> {}", current_word.name);
+                    let mut input = String::new();
+                    stdin().read_line(&mut input).unwrap();
+                    let answer = input.trim();
+                    match answer {
+                        //todo
+                        // _prompt 给出例句提示
+                        // _skip 跳过当前复习的单词, 不给答案, 不算次数
+                        // _cheat 直接给出答案, 不算次数
+                        "_exit" => break,//放弃复习
+                        _ => {
+                            if supervisor.exam(&answer) {
+                                println!("回答正确");
+                                supervisor.set_word_reviewed_once();
+                            } else {
+                                println!("回答内容是:{}", answer);
+                                println!("回答错误，正确答案释义是: {}", current_word.exp_cn);
+                            }
+                            if supervisor.is_finished() {
+                                //todo 复习完毕之后输出总结，包括成功次数，失败次数，共复习次数
+                                println!("复习已完毕");
+                                break;
+                            } else {
+                                supervisor.forward();
+                            }
+                        }
+                    }
+                    //todo 采集需要复习的单词并且显示出来
+                    // let mut child = Command::new(command).spawn().unwrap();
+                    // child.wait();
+                } else {
+                    panic!("程序出错")
+                };
+            }
+        } else {
+            println!("无法识别的operation参数");
+        }
+    }
+
+
+    // let connection = establish_connection();
+    // create_word(&connection, "hello", serde_json::to_string(&values).unwrap().as_str());
+}
+
+
+pub fn create_word<'a>(conn: &SqliteConnection, name_field: &'a str, exp_cn_field: &'a str) {
+    use ragger_api::schema::words;
+    use ragger_api::schema::words::dsl::*;
+    let new_word = NewWord {
+        name: name_field,
+        exp_cn: exp_cn_field,
+    };
+    println!("要插入的数据是 {:?}", &new_word);
+    diesel::insert_into(words::table).values(new_word).execute(conn).expect("插入数据失败");
+    let word_results = words.filter(status.eq(0)).limit(10).load::<Word>(conn).expect("查询出错了");
+    for word in word_results {
+        println!("单词名称是:{}, 单词释义:{}", word.name, word.exp_cn);
+    }
+    // println!("查询到了{}条数据", word_results.len());
+}
+
+
+
+
